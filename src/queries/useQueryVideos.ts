81c6ae799195d7a2ef5459ca2f2b33f3923ca7ac
shuta <baw.keepon69@gmail.com>
2023-04-09 22:41:20 +0900

    🔥 remove query folder

--- src/queries/useQueryVideos.ts
+++
@@ -1,8 +0,0 @@
-import { supabase } from "../scripts/supabase";
-
-export const useQueryVideos = async () => {
-  return await supabase
-    .from("videos")
-    .select("*")
-    .order("created_at", { ascending: true });
-};
